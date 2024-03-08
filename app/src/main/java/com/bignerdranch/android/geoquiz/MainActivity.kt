3031f788462bd9306122fef9f4de9bb6406ccbb6
daydaywynn <dayday_wynn@alumni.baylor.edu>
2024-03-08 11:52:31 -0600

    Updating GeoQuiz Project, Giving a score to the Quiz and resetting where necessary

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -10,11 +10,12 @@ import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 /**
- * Log modes: Error, Warning, Info, Debug
+ * Log modes: Error, Warning, Info, Debug, Verbose
  * Log.e(..) for Errors
  * Log.w(..) for Warnings
  * Log.i(..) for Info
  * Log.d(..) for Debugs
+ * Log.v(..) for Verbose (Development only)
  */
 private const val TAG : String = "MainActivity"
 class MainActivity : AppCompatActivity(){
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity(){
         Question(R.string.fourth_question, false)
     )
     private var curIndex = 0
+    private var answerArray : Array<Boolean?> = arrayOfNulls(questionBank.size)
+    private var answeredQuestions = 0
+    private var rightAnswers = 0
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
 
@@ -37,6 +41,9 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.QuestionsTextView.setText(questionBank[curIndex].textResId) //to get the id of the string and set the string as the text for TextView
+
+
+
         //Set listeners inside of the onCreate
         binding.ButtonTrue.setOnClickListener{view : View -> //'view' is the button that its listening to!
             /*
@@ -47,16 +54,21 @@ class MainActivity : AppCompatActivity(){
                 Snackbar.make(
                     view,
                     R.string.toast_false,
-                    Toast.LENGTH_LONG
+                    Snackbar.LENGTH_LONG
                 ).show() //Toast
             }
             else{
                 Snackbar.make(
                     view,
                     R.string.toast_true,
-                    Toast.LENGTH_LONG
+                    Snackbar.LENGTH_LONG
                 ).show() //Toast
             }
+            answerArray[curIndex] = true
+            binding.ButtonTrue.isClickable = false
+            binding.ButtonFalse.isClickable = true
+            answeredQuestions++
+            checkIfFinished(view)
         }
 
         binding.ButtonFalse.setOnClickListener{view : View ->
@@ -74,6 +86,13 @@ class MainActivity : AppCompatActivity(){
                     Toast.LENGTH_LONG
                 ).show() //Toast
             }
+            binding.ButtonFalse.isClickable = false
+            binding.ButtonTrue.isClickable = true
+
+            answeredQuestion()
+            answerArray[curIndex] = false
+            answeredQuestions++
+            checkIfFinished(view)
         }
 
         binding.QuestionsTextView.setOnClickListener { view: View->
@@ -90,8 +109,21 @@ class MainActivity : AppCompatActivity(){
         }
 
     }
+
+    private fun checkIfFinished(view: View) {
+        if (answeredQuestions.equals(questionBank.size)) {
+            Snackbar.make(
+                view,
+                "Your score is ${(rightAnswers / questionBank.size).toDouble()}%",
+                Snackbar.LENGTH_LONG
+            ).show()
+            resetAll()
+        }
+    }
+
     override fun onStart(){
         super.onStart()
+        resetButtons()
         Log.d(TAG, "onStart() called")
     }
 
@@ -114,6 +146,9 @@ class MainActivity : AppCompatActivity(){
         super.onDestroy()
         Log.d(TAG, "onDestroy called")
     }
+
+
+
     private fun updateQuestion(direction : Char){
         if(direction.equals('+')) {
             curIndex = (curIndex + 1) % questionBank.size
@@ -127,7 +162,35 @@ class MainActivity : AppCompatActivity(){
                 curIndex = (curIndex - 1) % questionBank.size
             }
             binding.QuestionsTextView.setText(questionBank[curIndex].textResId)
+
         }
+        resetButtons()
+        if(answerArray[curIndex] != null){ //question has an answer
+            answeredQuestion()
+            binding.ButtonTrue.isClickable = false
+            binding.ButtonFalse.isClickable = false
+        }
+    }
+
+    private fun resetButtons(){
+        binding.ButtonNext.isClickable = false
+        binding.ButtonPrev.isClickable = false
+        binding.ButtonTrue.isClickable = true
+        binding.ButtonFalse.isClickable = true
+    }
+
+    private fun answeredQuestion() {
+        binding.ButtonNext.isClickable = true
+        binding.ButtonPrev.isClickable = true
+    }
+
+    private fun resetAll(){
+        resetButtons()
+        answeredQuestions = 0
+        rightAnswers = 0
+        curIndex = 0
+        binding.QuestionsTextView.setText(questionBank[curIndex].textResId)
+        answerArray = arrayOfNulls(questionBank.size)
     }
 
 
